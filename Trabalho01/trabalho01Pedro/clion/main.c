5e45ffbdcc508dc328bf074646d45f8ebed36ed7
Pedroklemensov <pedroklemensov@gmail.com>
2023-04-19 16:48:27 -0300

    a

--- Trabalho01/trabalho01Pedro/clion/main.c
+++ Trabalho01/trabalho01Pedro/clion/main.c
@@ -1,57 +1,4 @@
-<<<<<<< Updated upstream
-#include <stdio.h>
-
-typedef struct PCB PCB; // declaração avançada da struct PCB
- struct PCB
-{
-    int pid;
-    int chegada;
-    int burst;
-    int prioridade;
-     //o prev e  next sao ponteiros para que eles possam apontar a memoria do seus PCBS
-     PCB* prev;
-     PCB* next;
-};
-
-int PROCESS = 5;
-int lista_de_PCBS [];
-int lista_de_burst [];
-
-for (int i= 0; i == PROCESS ; i++){
-
-}
-
-
-
-int main() {
-    // esse e o contrutor, para eu pegar o ponteiro de qualquer pcb ele tem que que ja te sido criado
-    // entao n posso coloacar o netx pcb2 no pcb1 pq ele n foi criado ainda
-    // para como o next e o prev sao pointeiros tem que coloacar o "&" antes declarar qual o pcb qual
-    //pcb eles sao ligado EX:"&pcb1"
-    PCB pcb1 = {1,0,2,1,NULL,NULL};
-    PCB pcb2 = {2,0,2,2,&pcb1,NULL};
-    //agora que o pcb2 foi criado eu declari ele como next do pcb1
-    pcb1.next=&pcb2;
-
-
-    //toda esse seçao de printes sao para testes  se os pcbs eseatao oks
-    // es para printar os dados do pcb tem que sequir esse estrutura no "%d\n", pcb#.(o dado que vc quer)
-    // caso queria o local da memoria usar p no lugar do d
-    printf("%p\n", pcb1);// local da memoria do pcb1
-    printf("%d\n", pcb1.pid);// pid do pcb1
-    printf("%p\n", pcb2.prev);//local da memoria do prev do pcb2(PCB1)
-    printf("%d\n", pcb2.pid);//pid do pcb2
-    printf("%d\n", pcb2.prev->pid);// pegando o pcb do prev do 2
-    printf("ohayo sekai,Good morning world !\n");// apenas um ola mundo
-    return 0;
-
-
-
-
-
-
-}
-=======
+//<<<<<<< Updated upstream
 //#include <stdio.h>
 //
 //typedef struct PCB PCB; // declaração avançada da struct PCB
@@ -66,7 +13,13 @@ int main() {
 //     PCB* next;
 //};
 //
+//int PROCESS = 5;
+//int lista_de_PCBS [];
+//int lista_de_burst [];
 //
+//for (int i= 0; i == PROCESS ; i++){
+//
+//}
 //
 //
 //
@@ -81,7 +34,9 @@ int main() {
 //    pcb1.next=&pcb2;
 //
 //
-//    //
+//    //toda esse seçao de printes sao para testes  se os pcbs eseatao oks
+//    // es para printar os dados do pcb tem que sequir esse estrutura no "%d\n", pcb#.(o dado que vc quer)
+//    // caso queria o local da memoria usar p no lugar do d
 //    printf("%p\n", pcb1);// local da memoria do pcb1
 //    printf("%d\n", pcb1.pid);// pid do pcb1
 //    printf("%p\n", pcb2.prev);//local da memoria do prev do pcb2(PCB1)
@@ -90,5 +45,50 @@ int main() {
 //    printf("ohayo sekai,Good morning world !\n");// apenas um ola mundo
 //    return 0;
 //
+//
+//
+//
+///typedef struct PCB PCB; // declaração avançada da struct PCB
+////// struct PCB
+//////{
+//////    int pid;
+//////    int chegada;
+//////    int burst;
+//////    int prioridade;
+//////     //o prev e  next sao ponteiros para que eles possam apontar a memoria do seus PCBS
+//////     PCB
+//
 //}
->>>>>>> Stashed changes
+//=======
+////#include <stdio.h>
+////
+///* prev;
+////     PCB* next;
+////};
+////
+////
+////
+////
+////
+////int main() {
+////    // esse e o contrutor, para eu pegar o ponteiro de qualquer pcb ele tem que que ja te sido criado
+////    // entao n posso coloacar o netx pcb2 no pcb1 pq ele n foi criado ainda
+////    // para como o next e o prev sao pointeiros tem que coloacar o "&" antes declarar qual o pcb qual
+////    //pcb eles sao ligado EX:"&pcb1"
+////    PCB pcb1 = {1,0,2,1,NULL,NULL};
+////    PCB pcb2 = {2,0,2,2,&pcb1,NULL};
+////    //agora que o pcb2 foi criado eu declari ele como next do pcb1
+////    pcb1.next=&pcb2;
+////
+////
+////    //
+////    printf("%p\n", pcb1);// local da memoria do pcb1
+////    printf("%d\n", pcb1.pid);// pid do pcb1
+////    printf("%p\n", pcb2.prev);//local da memoria do prev do pcb2(PCB1)
+////    printf("%d\n", pcb2.pid);//pid do pcb2
+////    printf("%d\n", pcb2.prev->pid);// pegando o pcb do prev do 2
+////    printf("ohayo sekai,Good morning world !\n");// apenas um ola mundo
+////    return 0;
+////
+////}
+//>>>>>>> Stashed changes
--- Trabalho01/trabalho01Pedro/clion/main2.c
+++ Trabalho01/trabalho01Pedro/clion/main2.c
@@ -1,211 +1,355 @@
-<<<<<<< Updated upstream
-
-=======
+//Alunos: Pedro Alexsander, Alberto Huarastaca ,Lucas Cavalcante Fontenele
+//
+//
+//
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define PROCESS 5
 
+//[2]
 typedef struct PCB {
     int pid;
     int chegada;
     int burst;
-    int prioridade;
-    struct PCB* prev;
-    struct PCB* next;
-    struct PCB* dad;
-} PCB;
+    float prioridade;
 
-int PROCESS = 5;
-PCB lista_de_PCBS[5];
-int lista_de_burst[5];
+    struct PCB *prev;
+    struct PCB *next;
+    struct PCB *dad; // ponteiro desnecessario?
+}PCB;
 
-typedef struct FCFS {
+//[5]
+
+/*typedef struct FCFS {
+//cria��o da estrutura
+    PCB *head; // pode ser feito sem a necessidade do struct, pois eles s� armazenam o valor de in�cio (cabe�a0 e fim (cauda), diferente do PCB. que referenciava a s� mesmo dentro da classe.
+    PCB *tail;
+    int size;
+}FCFS;
+*/
+
+typedef struct FCFS FCFS;
+struct FCFS{
     PCB* head;
     PCB* tail;
     int size;
-} FCFS;
+};
 
-void push(FCFS* fila, PCB* pcb) {
+void init (FCFS *fila){
+    fila->head = NULL;
+    fila->tail = NULL;
+    fila->size = 0;
+}
+
+void push_FCFS(FCFS *fila, PCB *pcb){
     if (fila->size == 0) {
         fila->head = pcb;
-        fila->tail = fila->head;
+        fila->tail = pcb;
     } else {
         fila->tail->next = pcb;
         fila->tail = fila->tail->next;
     }
-    fila->size++;
+    fila->size += 1;
 }
 
-PCB* pop(FCFS* fila) {
-    if (fila->size == 0) {
+PCB* pop(FCFS *fila){
+    if (fila->size == 0){
         return NULL;
     }
     PCB* temp = fila->head;
     fila->head = fila->head->next;
-    fila->size--;
+    fila->size -= 1;
     return temp;
 }
 
+//[11]
+typedef struct {
+    PCB **tree;
+    int size;
+}PRIORIDADE;
 
-
-typedef struct Prioridade {
-    PCB* tree[10];
-}Prioridade;
-
-Prioridade* swap(Prioridade self, int i , int j) {
-    Prioridade* temp = self.tree[i];
-    self.tree[i] = self.tree[j];
-    self.tree[j]=temp;
+void swap(PCB **tree, int i, int j){
+    PCB *temp = tree[i];
+    tree[i] = tree[j];
+    tree[j] = temp;
 }
 
-Prioridade* go_down(Prioridade self, int i){
+void go_down(PRIORIDADE *prioridade, int i){
     int j = (i + 1) * 2;
-    int n = sizeof(self.tree);
-    if (j <= n) {
-        if( j < n) {
-            if (self.tree[j]->prioridade < self.tree[j - 1]->prioridade) {
+    int n = prioridade->size;
+
+    if (j <= n){
+        if (j < n){
+            if (prioridade->tree[j]->prioridade < prioridade->tree[j - 1]->prioridade){
                 j++;
-            }
-        }
-        if (self.tree[i]->prioridade > self.tree[j-1]->prioridade) {
-            swap(self , i , j-1);
-            go_down(self, j-1);
         }
     }
-}
-
-Prioridade* go_up (Prioridade self, int i) {
-    if (i == 0) {
-        return 0;
-    }
-    int j = (i - 1) / 2;   // era um comentario de dois ou dividido por 2 ou multiplizar por 2?
-    if (self.tree[i]->prioridade < self.tree[j]->prioridade) {
-        swap(self, i, j);
-        go_up(self, j);
-    }
-    return 0;
-
-}
-
-Prioridade* push_prioridade(Prioridade self, PCB node) {
-    self.tree[0]= &node;
-    if (sizeof(self.tree) == 1) {
-        return 0;
-    }else{
-        go_up(self, sizeof(self.tree) - 1);
-        return 0;
+    else if (prioridade->tree[i]->prioridade > prioridade->tree[j - 1]->prioridade){
+        swap(prioridade->tree, i, j - 1);
+        go_down(prioridade, j - 1);
+        }
     }
 }
 
 
+void go_up(PRIORIDADE *prioridade, int i){
+    if (i == 0){
+    return;
+}
+int j = (i - 1) / 2;
+    if (prioridade->tree[i]->prioridade < prioridade->tree[j]->prioridade){
+        swap(prioridade->tree, i, j);
+        go_up(prioridade, j);
+}
+}
 
-Prioridade* pop_prioridade(Prioridade self) {
-    if (sizeof(self.tree) > 0) {
-        Prioridade* temp = self.tree[0];
-        self.tree[0] = self.tree[-1];
-        pop_prioridade(self);
-        go_down(self , 0);
+void push_Prioridade(PRIORIDADE *prioridade, PCB *pcb) {
+    prioridade->tree = (PCB **)realloc(prioridade->tree, (prioridade->size + 1) * sizeof(PCB *));
+    prioridade->tree[prioridade->size] = pcb;
+    prioridade->size++;
+    if (prioridade->size > 1) {
+        go_up(prioridade, prioridade->size - 1);
+    }
+}
+
+PCB* pop_Prioridade(PRIORIDADE *prioridade) {
+    if (prioridade->size > 0) {
+        PCB *temp = prioridade->tree[0];
+        prioridade->tree[0] = prioridade->tree[prioridade->size - 1];
+        prioridade->size--;
+        go_down(prioridade, 0);
         return temp;
     }
     return NULL;
 }
 
-
+float mean(float *lista) {
+    float s = 0;
+    for (int i = 0; i < PROCESS; i++) {
+        s += lista[i];
+    }
+    return s / PROCESS;
+}
 
 int main() {
-    srand((unsigned long)time(0));
+
+    srand(time(NULL));
+
+    FCFS fila;
+	init(&fila);
+
+    PCB* lista_PCBS[PROCESS];
+    float turnaround_FCFS[PROCESS];
+    float espera_FCFS[PROCESS];
+    int lista_Burst[PROCESS];
+
+
+
 
     int chegada = 0;
-    for (int i = 0; i < PROCESS; i++) {
-        int burst = rand() % 10;
-        int prioridade = rand() % 10;
+
+    for (int i = 0 ; i<PROCESS ; i++) {
+
+        int burst = rand() % 10 + 1;
+        int prioridade = rand() % 10 + 1;
+
         if (i < 2) {
-            chegada = 0;
+            chegada = rand() % 10 + 1;
         } else {
             chegada = i - 1;
         }
-        lista_de_PCBS[i].pid = i;
-        lista_de_PCBS[i].chegada = chegada;
-        lista_de_PCBS[i].burst = 0;
-        lista_de_PCBS[i].prioridade = prioridade;
-        lista_de_PCBS[i].prev = NULL;
-        lista_de_PCBS[i].next = NULL;
-        lista_de_PCBS[i].dad = NULL;
-        lista_de_burst[i] = burst;
+
+        PCB *pcb = (PCB *)malloc(sizeof(PCB));
+        pcb->pid = i;
+        pcb->chegada = chegada;
+        pcb->burst = 0;
+        pcb->prioridade = prioridade;
+        pcb->prev = NULL;
+        pcb->next = NULL;
+        pcb->dad = NULL;
+
+        lista_PCBS[i] = pcb;
+        lista_Burst[i] = burst;
     }
 
-    FCFS fila;
-    fila.head = NULL;
-    fila.tail = NULL;
-    fila.size = 0;
 
-    int turnaround_FCFS[5];
-    int espera_FCFS[5];
 
-    for (int i = 0; i < PROCESS; i++) {
-        lista_de_PCBS[i].burst = lista_de_burst[i];
-        espera_FCFS[i] = -lista_de_burst[i];
-        turnaround_FCFS[i] = 0;
-        push(&fila, &lista_de_PCBS[i]);
+    for (int i = 0; i < PROCESS ; i++) {
+
+        lista_PCBS[i]->burst = lista_Burst[i];
+        espera_FCFS[i]= - lista_Burst[i];
+        turnaround_FCFS[i]= 0;
+        push_FCFS(&fila,lista_PCBS[i]);
     }
 
-    PCB* node = fila.head;
+
+    PCB* node_FCFS = fila.head;
+    while (node_FCFS != NULL) {
+        printf("PID:%d|CHEGADA:%d|BURST:%d\n", node_FCFS->pid, node_FCFS->chegada, node_FCFS->burst);
+        node_FCFS = node_FCFS->next;
+    }
+
+
+    node_FCFS = pop(&fila);
+    int t = 0;
+    while(node_FCFS != NULL) {
+        while (node_FCFS->burst > 0){
+                        printf("t:%d, PID:%d|BURST:%d\n", t, node_FCFS->pid, node_FCFS->burst);
+            t++;
+            node_FCFS->burst -= 1;
+            if (node_FCFS->burst == 0) {
+                turnaround_FCFS[node_FCFS->pid] = t - node_FCFS->chegada;
+                espera_FCFS[node_FCFS->pid] += turnaround_FCFS[node_FCFS->pid];
+            }
+        }
+        node_FCFS=pop(&fila);
+    }
+    printf("Fim do FCFS \n");
+    printf("-------------------------------------\n");
+
+    float turnaround_PRIORIDADE[PROCESS];
+    float espera_PRIORIDADE[PROCESS];
+    PRIORIDADE fila_prior_1;
+    fila_prior_1.tree = NULL;
+    fila_prior_1.size = 0;
+
+    chegada = 0;
+
+    for (int i=0;i<PROCESS;i++) {
+
+        lista_PCBS[i]->burst = lista_Burst[i];
+        espera_PRIORIDADE[i]= - lista_Burst[i];
+        turnaround_PRIORIDADE[i] = 0;
+        push_Prioridade(&fila_prior_1 , lista_PCBS[i]);
+
+    }
+
+
+
+    for (int i=0;i<PROCESS;i++) {
+    lista_PCBS[i]->burst = lista_Burst[i];
+    push_Prioridade(&fila_prior_1 , lista_PCBS[i]);
+    }
+    for (int i =0 ; i < PROCESS; i++) {
+        printf("PID:%d|CHEGADA:%d|BURST:%d|PRIORIDADE:%f\n", fila_prior_1.tree[i]->pid , fila_prior_1.tree[i]->chegada , fila_prior_1.tree[i]->burst , fila_prior_1.tree[i]->prioridade);
+
+    }
+
+    PCB *node = pop_Prioridade(&fila_prior_1);
+    t = 0;
+    while (node != NULL) {
+        while (node->burst > 0) {
+            printf("t:%d, PID:%d|BURST:%d\n", t, node->pid, node->burst);
+            t++;
+            node->burst -=1;
+            if (node->burst == 0) {
+                turnaround_PRIORIDADE[node->pid] = t - node->chegada;
+                espera_PRIORIDADE[node->pid] += turnaround_PRIORIDADE[node->pid];
+            }
+        }
+        node = pop_Prioridade(&fila_prior_1);
+    }
+    printf("Fim do PRIORIDADE \n");
+    printf("-------------------------------------\n");
+
+    float turnaround_SJF[PROCESS];
+    float espera_SJF[PROCESS];
+    PRIORIDADE fila_SJF;
+    fila_SJF.tree = NULL;
+    fila_SJF.size = 0;
+
+    for (int i = 0; i < PROCESS ; i++) {
+        lista_PCBS[i]->burst = lista_Burst[i];
+        espera_SJF[i]= - lista_Burst[i];
+        turnaround_SJF[i] = 0;
+        PCB *node = lista_PCBS[i];
+        node->prioridade = 1 - 1 / (float)node->burst;
+        push_Prioridade(&fila_SJF , node);
+    }
+
+    for (int i = 0 ; i < PROCESS ; i++){
+        printf("PID:%d|CHEGADA:%d|BURST:%d|PRIORIDADE:%f\n", fila_SJF.tree[i]->pid , fila_SJF.tree[i]->chegada , fila_SJF.tree[i]->burst , fila_SJF.tree[i]->prioridade);
+
+    }
+
+    //problema abaixo;;
+
+    node = pop_Prioridade(&fila_SJF);
+    t=0;
+    while (node != NULL) {
+        while (node->burst > 0) {
+            printf("t:%d, PID:%d|BURST:%d\n", t, node->pid, node->burst);
+            t++;
+            node->burst -=1;
+            if (node->burst == 0) {
+                turnaround_SJF[node->pid] = t - node->chegada;
+                espera_SJF[node->pid] += turnaround_SJF[node->pid];
+            }
+        }
+        node=pop_Prioridade(&fila_SJF);
+    }
+
+    printf("Fim do SJF \n");
+    printf("-------------------------------------\n");
+
+    float turnaround_RR[PROCESS];
+    float espera_RR[PROCESS];
+    FCFS fila_rr;
+    init(&fila_rr);
+
+    for (int i=0;i<PROCESS;i++){
+        lista_PCBS[i]->burst = lista_Burst[i];
+        espera_RR[i]= - lista_Burst[i];
+        turnaround_RR[i] = 0;
+        push_FCFS(&fila_rr,lista_PCBS[i]);
+    }
+
+    node = fila_rr.head;
     while (node != NULL) {
         printf("PID:%d|CHEGADA:%d|BURST:%d\n", node->pid, node->chegada, node->burst);
         node = node->next;
     }
 
-    node = pop(&fila);
-    int t = 0;
-    while (node != NULL) {
-        while (node->burst > 0) {
+    node = pop(&fila_rr);
+    t=0;
+    while(node != NULL) {
+        int quantum = 1;
+        while (quantum > 0) {
             printf("t:%d, PID:%d|BURST:%d\n", t, node->pid, node->burst);
-            t++;
-            node->burst--;
-            if (node->burst == 0) {
-                turnaround_FCFS[node->pid] = t - node->chegada;
-                espera_FCFS[node->pid] += turnaround_FCFS[node->pid];
+            t += 1;
+            node->burst -= 1;
+            if (node->burst == 0 ) {
+                turnaround_RR[node->pid] = t - node->chegada;
+                espera_RR[node->pid] += turnaround_RR[node->pid];
+                break;
             }
+            quantum -= 1;
         }
-        node = pop(&fila);
+        if (node->burst > 0){
+            push_FCFS(&fila_rr , node);
+        }
+        node = pop(&fila_rr);
     }
-
-
-    int turnaround_PRIORIDADE[5];
-    int espera_PRIORIDADE[5];
-    struct Prioridade fila_prio;
-    //fila.size=0;
-
-
-    for (int i = 0; i < PROCESS ; i++) {
-        lista_de_PCBS[i].burst = lista_de_burst[i];
-        espera_PRIORIDADE[i]= - lista_de_burst[i];
-        turnaround_PRIORIDADE[i]=0;
-        push_prioridade(fila_prio,lista_de_PCBS[i]);
-    }
-
-    for (int i = 0 ; i < PROCESS ; i++) {
-        lista_de_PCBS[i].burst = lista_de_burst[i];
-        push_prioridade(fila_prio , lista_de_PCBS[i]);
-    }
-
-    int i;
-    for(i = 0; i < 5 ; i++){
-        printf("PID:%d|CHEGADA:%d|BURST:%d|PRIORIDADE:%d\n", fila_prio.tree[i]->pid, fila_prio.tree[i]->chegada, fila_prio.tree[i]->burst,fila_prio.tree[i]->prioridade);
-    }
+    printf("Fim do RR \n");
+    printf("-------------------------------------\n");
 
 
 
 
+    printf("Turnaround FCFS:%f \n", mean(turnaround_FCFS));
+    printf("Turnaround PRIORIDADE:%f \n", mean(turnaround_PRIORIDADE));
+    printf("Turnaround SJF:%f \n", mean(turnaround_SJF));
+    printf("Turnaround RR:%f \n", mean(turnaround_RR));
+
+    printf("Espera FCFS:%f \n", mean(espera_FCFS));
+    printf("Espera PRIORIDADE:%f \n", mean(espera_PRIORIDADE));
+    printf("Espera SJF:%f \n", mean(espera_SJF));
+    printf("Espera RR:%f \n", mean(espera_RR));
 
 
-
-
-
-
-
-
-
-    return 0;
+printf("All good \n");
+return 0;
 }
->>>>>>> Stashed changes
+
+
